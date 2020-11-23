4a5af7280cb9c7df11af2c3197d55e685764bbcd
Tjen Wellens <tjen.wellens@gmail.com>
2020-11-23 17:20:56 +0100

    inline function

--- createStatementData.js
+++ createStatementData.js
@@ -10,7 +10,7 @@ export function createStatementData(invoice, plays) {
 		const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result
   }
@@ -19,11 +19,7 @@ export function createStatementData(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
-  function amountFor(aPerformance) {
-		return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-  }
-
-  function volumeCreditsFor(aPerformance) {
+	function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
     if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
