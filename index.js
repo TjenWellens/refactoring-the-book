7233f05369a42804c497d2b8e91fcd06a292a1dd
Tjen Wellens <tjen.wellens@gmail.com>
2020-11-23 15:51:38 +0100

    inline variable

--- index.js
+++ index.js
@@ -26,9 +26,8 @@ export default function statement(invoice, plays) {
 		totalAmount += amountFor(perf);
 	}
 
-	let volumeCredits = totalVolumeCredits();
 	result += `Amount owed is ${usd(totalAmount)}\n`;
-	result += `You earned ${volumeCredits} credits\n`;
+	result += `You earned ${(totalVolumeCredits())} credits\n`;
 	return result;
 
 	function totalVolumeCredits() {
