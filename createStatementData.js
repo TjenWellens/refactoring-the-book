d9d8c06056b0f0787fe72bcb74a886cd5921c67d
Tjen Wellens <tjen.wellens@gmail.com>
2020-11-23 17:39:30 +0100

    Replace Conditional with Polymorphism

--- createStatementData.js
+++ createStatementData.js
@@ -39,7 +39,6 @@ class PerformanceCalculator {
 	get volumeCredits() {
 		let result = 0;
 		result += Math.max(this.performance.audience - 30, 0);
-		if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
 		return result;
 	}
 }
@@ -72,6 +71,12 @@ class ComedyCalculator extends PerformanceCalculator {
 			result += 10000 + 500 * (this.performance.audience - 20);
 		}
 		result += 300 * this.performance.audience;
+		return result;
+	}
+
+	get volumeCredits() {
+		let result = super.volumeCredits;
+		result += Math.floor(this.performance.audience / 5);
 		return result;
 	}
 }
